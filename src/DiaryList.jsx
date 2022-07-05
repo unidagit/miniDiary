0e4093e117536196e890dd4f3362eecef084ea28
yunheejo <rhcjrdbsgml@gmail.com>
2022-07-05 14:45:48 +0900

    삭제하기 기능 추가

--- src/DiaryList.jsx
+++ src/DiaryList.jsx
@@ -1,13 +1,13 @@
 import DiaryItem from "./DiaryItem.jsx";
 
-const DiaryList = ({ diaryList }) => {
+const DiaryList = ({ onDelete, diaryList }) => {
   return (
     <div className="DiaryList">
       <h2>일기리스트</h2>
       <h4>{diaryList.length}개의 일기가 있습니다.</h4>
       <div>
         {diaryList.map((it) => (
-          <DiaryItem key={it.id} {...it} />
+          <DiaryItem key={it.id} {...it} onDelete={onDelete} />
         ))}
       </div>
     </div>
