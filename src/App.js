bd10e6e5d8082c74fac2deacbe4aeb72233e928c
yunheejo <rhcjrdbsgml@gmail.com>
2022-07-05 14:19:10 +0900

    더미데이터 삭제, 게시글 추가 함수 생성

--- src/App.js
+++ src/App.js
@@ -1,36 +1,30 @@
+import { useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 
-const dummyList = [
-  {
-    id: 1,
-    author: "하마",
-    content: "안녕 나는 하마",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 2,
-    author: "사자",
-    content: "안녕 나는 사자",
-    emotion: 4,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 3,
-    author: "두루미",
-    content: "안녕 나는 두루미",
-    emotion: 3,
-    created_date: new Date().getTime(),
-  },
-];
-
 const App = () => {
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
+
   return (
     <div className="App">
-      <DiaryEditor />
-      <DiaryList diaryList={dummyList} />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} />
     </div>
   );
 };
